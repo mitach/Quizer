f75e9013edd397374a9a8ea4ba444483eceb081a
mitach <malinovd02@gmail.com>
2022-05-02 14:09:34 +0300

    style: Added overall stilization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+const API_URL = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [questions, setQuestions] = useState([]);
+
+    return (
+        <div className="container">
+            <div className="bg-white text-purple-800 p-10 rounded-lg shadow-md">
+                <h2 className="text-2xl">This is where the question is</h2>
+            </div>
+
+
+            <div className="grid grid-cols-2 gap-6 mt-6">
+                <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">Answer 1</button>
+                <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">Answer 2</button>
+                <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">Answer 3</button>
+                <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">Answer 4</button>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
