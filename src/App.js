b626b8390663b70adb8fc7578cc07f3650408fc8
mitach <malinovd02@gmail.com>
2022-05-03 17:41:11 +0300

    Fix: Rerendering with shuffled questions fixed

--- src/App.js
+++ src/App.js
@@ -8,23 +8,41 @@ function App() {
     const [questions, setQuestions] = useState([]);
     const [currentIndex, setCurrentIndex] = useState(0);
     const [score, setScore] = useState(0);
+    const [showAnswers, setShowAnswers] = useState(false);
 
     useEffect(() => {
         fetch(API_URL)
             .then((res) => res.json())
             .then((data) => {
-                setQuestions(data.results);
+                const questions = data.results.map((question) =>
+                ({
+                    ...question,
+                    answers: [
+                        question.correct_answer,
+                        ...question.incorrect_answers
+                    ].sort(() => {
+                        Math.random()
+                    })
+                }))
+
+                setQuestions(questions);
             })
     }, [])
 
     const handleAnswer = (answer) => {
-        const newIndex = currentIndex + 1;
-
-        setCurrentIndex(newIndex);
-
-        if (answer === questions[currentIndex].correct_answer) {
-            setScore(score + 1);
+        if (!showAnswers) {
+            if (answer === questions[currentIndex].correct_answer) {
+                setScore(score + 1);
+            }
         }
+
+        setShowAnswers(true);
+    }
+
+    const handleNextQuestion = () => {
+        setCurrentIndex(currentIndex + 1);
+
+        setShowAnswers(false);
     }
 
     return questions.length > 0
@@ -33,7 +51,12 @@ function App() {
                 ? (
                     <h1 className="text-3xl text-white font-bold">Your score is: {score}.</h1>
                 ) : (
-                    <Questionaire data={questions[currentIndex]} handleAnswer={handleAnswer} />
+                    <Questionaire
+                        data={questions[currentIndex]}
+                        handleAnswer={handleAnswer}
+                        handleNextQuestion={handleNextQuestion}
+                        showAnswers={showAnswers}
+                    />
                 )}
         </div >
         : <h2 className="text-2xl text-white font-bold">Loading...</h2>
--- src/components/Questionaire.js
+++ src/components/Questionaire.js
@@ -1,25 +1,34 @@
 import React from "react";
 
-const Questionaire = ({ handleAnswer, data: { question, correct_answer, incorrect_answers }, }) => {
-
-    const shuffledAnswers = [correct_answer, ...incorrect_answers].sort(() => Math.random() - 0.5);
+const Questionaire = ({ showAnswers, handleAnswer, handleNextQuestion, data: { question, correct_answer, answers } }) => {
 
     return (
-        <div>
+        <div className="flex flex-col">
             <div className="bg-white text-purple-800 p-10 rounded shadow-md">
                 <h2 className="text-2xl" dangerouslySetInnerHTML={{ __html: question }} />
             </div>
 
             <div className="grid grid-cols-2 gap-6 mt-6">
-                {shuffledAnswers.map(answer => (
-                    <button
-                        className={`bg-white p-4 text-purple-800 font-semibold rounded shadow`}
-                        onClick={() => handleAnswer(answer)}
-                        key={answer}
-                        dangerouslySetInnerHTML={{ __html: answer }}
-                    />
-                ))}
+                {answers.map((answer, idx) => {
+                    const textColor = showAnswers ? answer === correct_answer ? 'text-green-500' : 'text-red-400' : 'text-purple-800'
+                    return (
+                        <button
+                            className={`${textColor} bg-white p-4 text-purple-800 font-semibold rounded shadow`}
+                            onClick={() => handleAnswer(answer)}
+                            key={idx}
+                            dangerouslySetInnerHTML={{ __html: answer }}
+                        />
+                    )
+                })}
             </div>
+            {showAnswers && (
+                <button
+                    className={`ml-auto mt-6 bg-purple-600 text-white p-4 font-semibold rounded shadow`}
+                    onClick={handleNextQuestion}
+                >
+                    Next Question
+                </button>
+            )}
         </div>
     );
 }
