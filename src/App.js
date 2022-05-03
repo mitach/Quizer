607512807635121a298055544f09b3ac9937781b
mitach <malinovd02@gmail.com>
2022-05-03 15:24:25 +0300

    build: Iterating through questions on answer clicks

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ const API_URL = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=eas
 function App() {
 
     const [questions, setQuestions] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [score, setScore] = useState(0);
 
     useEffect(() => {
         fetch(API_URL)
@@ -15,13 +17,27 @@ function App() {
             })
     }, [])
 
-    return (
-        questions.length > 0
-            ?   <div className="container">
-                    <Questionaire data={questions[0]} />
-                </div >
-            : <h2 className="text-2xl text-white font-bold">Loading...</h2>
-    );
+    const handleAnswer = (answer) => {
+        const newIndex = currentIndex + 1;
+
+        setCurrentIndex(newIndex);
+
+        if (answer === questions[currentIndex].correct_answer) {
+            setScore(score + 1);
+        }
+    }
+
+    return questions.length > 0
+        ? <div className="container">
+            {currentIndex >= questions.length
+                ? (
+                    <h1 className="text-3xl text-white font-bold">Your score is: {score}.</h1>
+                ) : (
+                    <Questionaire data={questions[currentIndex]} handleAnswer={handleAnswer} />
+                )}
+        </div >
+        : <h2 className="text-2xl text-white font-bold">Loading...</h2>
+
 }
 
 export default App;
